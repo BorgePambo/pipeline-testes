74716add91b6f822dcd7bdb19ddda8e5b54d09dd
Borge Pambo <borginhopambo01@gmail.com>
2026-02-10 20:56:59 -0300

    fix import path nos testes

--- testes/test_etl.py
+++ testes/test_etl.py
@@ -1,10 +1,22 @@
+import os
+import sys
 import pandas as pd
+
+# 👉 adiciona a pasta raiz do projeto no PYTHONPATH
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..")
+    )
+)
+
 from etl_pipeline import extrair_dados, transformar_dados
+
 
 def test_extracao():
     df = extrair_dados()
     assert not df.empty
     assert all(col in df.columns for col in ['nome', 'idade', 'email', 'salario'])
+
 
 def test_transformacao():
     df = pd.DataFrame({'idade': [10, -5, 20]})
